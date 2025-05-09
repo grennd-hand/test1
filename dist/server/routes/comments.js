d4252073e15237429b073eaf4c084b8fb301b2b8
xiaomi <2149732023@qq.com>
2025-05-10 00:38:16 +0800

    chore: remove node_modules and update package files

--- dist/server/routes/comments.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-// ʾ��·��
-router.get('/', (req, res) => {
-    res.json({ msg: '����·������' });
-});
-export default router;
--- dist/server/routes/upload.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-router.post('/', (req, res) => {
-    res.json({ msg: '�ϴ�·������' });
-});
-export default router;
